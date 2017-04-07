b1d2928a35c9f6a85c8113c05614debc4dbf37f5
DeltaSkiez <wolloyo@gmail.com>
2017-04-07 19:08:32 -0400

    Create server.js

---
+++ chat-plugins/server.js
@@ -0,0 +1,91 @@
+'use strict';
+
+function clearRoom(room) {
+	let len = (room.log && room.log.length) || 0;
+	let users = [];
+	while (len--) {
+		room.log[len] = '';
+	}
+	for (let u in room.users) {
+		users.push(u);
+		Users(u).leaveRoom(room, Users(u).connections[0]);
+	}
+	len = users.length;
+	setTimeout(() => {
+		while (len--) {
+			Users(users[len]).joinRoom(room, Users(users[len]).connections[0]);
+		}
+	}, 1000);
+}
+
+exports.commands = {
+	gclearall: 'globalclearall',
+	globalclearall: function (target, room, user) {
+		if (!this.can('gdeclare')) return false;
+
+		Rooms.rooms.forEach(room => clearRoom(room));
+		Users.users.forEach(user => user.popup('All rooms have been cleared.'));
+		this.privateModCommand(`(${user.name} used /globalclearall.)`);
+	},
+	
+	dm: 'daymute',
+	daymute: function (target, room, user, connection, cmd) {
+		if (!target) return this.errorReply("Usage: /dm [user], [reason].");
+		if (!this.canTalk()) return this.sendReply("You cannot do this while unable to talk.");
+
+		target = this.splitTarget(target);
+		let targetUser = this.targetUser;
+		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
+		if (target.length > 300) {
+			return this.sendReply("The reason is too long. It cannot exceed 300 characters.");
+		}
+
+		let muteDuration = 24 * 60 * 60 * 1000;
+		if (!this.can('mute', targetUser, room)) return false;
+		let canBeMutedFurther = ((room.getMuteTime(targetUser) || 0) <= (muteDuration * 5 / 6));
+		if ((room.isMuted(targetUser) && !canBeMutedFurther) || targetUser.locked || !targetUser.connected) {
+			let problem = " but was already " + (!targetUser.connected ? "offline" : targetUser.locked ? "locked" : "muted");
+			if (!target) {
+				return this.privateModCommand("(" + targetUser.name + " would be muted by " + user.name + problem + ".)");
+			}
+			return this.addModCommand("" + targetUser.name + " would be muted by " + user.name + problem + "." + (target ? " (" + target + ")" : ""));
+		}
+
+		if (targetUser in room.users) targetUser.popup("|modal|" + user.name + " has muted you in " + room.id + " for 24 hours. " + target);
+		this.addModCommand("" + targetUser.name + " was muted by " + user.name + " for 24 hours." + (target ? " (" + target + ")" : ""));
+		if (targetUser.autoconfirmed && targetUser.autoconfirmed !== targetUser.userid) this.privateModCommand("(" + targetUser.name + "'s ac account: " + targetUser.autoconfirmed + ")");
+		this.add('|unlink|' + toId(this.inputUsername));
+
+		room.mute(targetUser, muteDuration, false);
+	},
+	
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
+
+		let pmName = ' Server Info [Do Not Respond!]';
+		Users.users.forEach(curUser => {
+			let message = '|pm|' + pmName + '|' + curUser.getIdentity() + '|' + target;
+			curUser.send(message);
+		});
+	},
+	pmallhelp: ["/pmall [message]."],
+
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		let pmName = ' InFo.Staff';
+
+		Users.users.forEach(curUser => {
+			if (!curUser.isStaff) return;
+			let message = '|pm|' + pmName + '|' + curUser.getIdentity() + '|' + target;
+			curUser.send(message);
+		});
+	},
+	pmallstaffhelp: ["/pmallstaff [message]"],
+		
+};
