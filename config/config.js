90157e2ac4961642cda33251abd2a8bac93dabaf
DeltaSkiez <wolloyo@gmail.com>
2017-04-08 08:37:26 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -212,8 +212,8 @@ exports.replsocketmode = 0o600;
 
 //github repo config
 exports.github = {
-	secret: "", // Your repo secret
-	port: "", // Desired port, must be unused and above 1000
+	secret: "1080", // Your repo secret
+	port: "1200", // Desired port, must be unused and above 1000
 	rooms: ['development'], // Desired rooms
 };
 
