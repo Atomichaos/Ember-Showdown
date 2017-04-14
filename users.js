b94414a4351e2ae0d2e5e31b0ca90d1625606a8a
DeltaSkiez <wolloyo@gmail.com>
2017-04-14 12:43:58 -0400

    Update users.js

--- users.js
+++ users.js
@@ -43,28 +43,6 @@ function isHoster(user) {
 	if (hoster === 1) return true;
 	return false;
 }
-function generateNews () {
-			let lobby = Rooms('lobby');
-			if (!lobby) return false;
-			if (!lobby.news || Object.keys(lobby.news).length < 0) return false;
-			if (!lobby.news) lobby.news = {};
-			let news = lobby.news, newsDisplay = [];
-			Object.keys(news).forEach(announcement => {
-				newsDisplay.push(`<h4>${announcement}</h4>${news[announcement].desc}<br /><br /><strong>—<font color="${EM.Color(news[announcement].by)}">${news[announcement].by}</font></strong> on ${moment(news[announcement].posted).format("MMM D, YYYY")}`);
-			});
-			return newsDisplay;
-		}
-
-function newsDisplay(user) {
-			if (!Users(user)) return false;
-			let newsDis = generateNews();
-			if (newsDis.length === 0) return false;
-
-			if (newsDis.length > 0) {
-				newsDis = newsDis.join('<hr>');
-				return Users(user).send(`|pm| Ember News|${Users(user).getIdentity()}|/raw ${newsDis}`);
-			}
-}
 
 /*********************************************************
  * Users map
@@ -648,7 +626,7 @@ class User {
 		} else {
 			this.send(`|nametaken|${name}|Your authentication token was invalid.`);
 		}
-
+		EM.showNews(userid, this);
 		return false;
 	}
 	validateRename(name, tokenData, newlyRegistered, challenge) {
