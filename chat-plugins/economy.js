98cbc92c780dc3a060e48327471a434069922e84
DeltaSkiez <wolloyo@gmail.com>
2017-04-14 10:53:47 -0400

    Update economy.js

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -62,7 +62,7 @@ let shopDisplay = getShopDisplay(shop);
  * @return {String} display
  */
 function getShopDisplay(shop) {
-	let display = "<center><img src=http://i.imgur.com/1vXo5UW.png><img src=http://i.imgur.com/tXlUyas.png width=250> <img src=http://i.imgur.com/1vXo5UW.png></center><br><div' + (!this.isOfficial ? ' class=infobox-limited' : '') + '><table style='background: #000; border-color: #01D0DF; border-radius: 8px' border='1' cellspacing='0' cellpadding='5' width='100%'>" +
+	let display = "<center><img src=http://i.imgur.com/1vXo5UW.png><img src=http://i.imgur.com/rYmNctD.png width=250> <img src=http://i.imgur.com/1vXo5UW.png></center><br><div' + (!this.isOfficial ? ' class=infobox-limited' : '') + '><table style='background: #000; border-color: #01D0DF; border-radius: 8px' border='1' cellspacing='0' cellpadding='5' width='100%'>" +
 		"<tbody><tr><th><font color=#01D0DF face=courier>Item</font></th><th><font color=#01D0DF face=courier>Description</font></th><th><font color=#01D0DF face=courier>Price</font></th></tr>";
 	let start = 0;
 	while (start < shop.length) {
